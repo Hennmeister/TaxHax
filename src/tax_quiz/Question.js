a71c397fce1df0db9d700510b7188bbf6d0fa64e
Hennmeister <henning.lindig@rogers.com>
2020-11-21 16:47:43 -0500

    basic quiz logic

--- src/tax_quiz/Question.js
+++ src/tax_quiz/Question.js
@@ -4,11 +4,27 @@ import classes from './Question.module.css'
 import { Box, Button } from '@material-ui/core'
 
 export default class Question extends Component {
+    state = {
+        InAnimation: false,
+    }
+
+    clickHandler = () => {
+        this.setState({InAnimation: true}, () => setTimeout(() => {
+            this.setState({InAnimation: false})
+            this.props.responseHandler()
+        }, 750))
+    }
+
     render() {
+        let boxClasses = [ classes.QuestionBox ]
+        if(this.state.InAnimation) {
+            boxClasses.push(classes.SlideTransition)
+        }
+
         const responseButtons =
             this.props.responseType === 'yn' ? (
                 <Box>
-                    <Button>Yes</Button>
+                    <Button onClick={this.clickHandler}>Yes</Button>
                     <Button>No</Button>
                 </Box>
             ) : (
@@ -16,7 +32,7 @@ export default class Question extends Component {
             )
 
         return (
-            <Box>
+            <Box className={boxClasses.join(' ')}>
                 <h1 className={classes.Question}>{this.props.q}</h1>
                 <h2>Secondary header</h2>
                 {responseButtons}
---
+++ src/tax_quiz/QuestionList.js
@@ -0,0 +1,8 @@
+const questionList = [
+    {question: "Are You Over 18?", options: ["Yes", "No"]},
+    {question: "Sample Question?", options: ["Yes", "No"]},
+    {question: "Sample Question?", options: ["Yes", "No"]},
+    {question: "Sample Question?", options: ["Yes", "No"]},
+ ]
+
+ export default questionList
