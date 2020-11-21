9f5b2d2a1cda8627819f0d93171d30f14ca4ebbf
Tilman Lindig <tilman.lindig@rogers.com>
2020-11-21 11:51:59 -0500

    add linting preferences

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import LandingPage from './landing_page/LandingPage';
+import logo from './logo.svg'
+import './App.css'
+import LandingPage from './landing_page/LandingPage'
 
 function App() {
-  return (
-      <LandingPage></LandingPage>
-  );
+    return <LandingPage></LandingPage>
 }
 
-export default App;
+export default App
